224cb9c4eddf06da78b8b66baf5ff568c171fed8
Antti Jankeri <antti.jankeri@gmail.com>
2023-11-16 11:11:09 +0100

    feat: move route files + error handler

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
 
@@ -12,5 +12,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use("/api/v1", indexRouter);
+
+const errorHandler = (
+  err: Error,
+  _req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+  res.status(500);
+  res.render("error", { error: err });
+};
+
+app.use(errorHandler);
 
 export default app;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,19 @@
 import express from "express";
-import auth from "./auth";
+
 import users from "./users";
+import auth from "./auth";
 import objects from "./objects";
-import pics from "./pics";
+import images from "./images";
+import dataExport from "./dataExport";
+import dataShare from "./dataShare";
 
 const router = express.Router();
 
 router.use("/auth", auth);
 router.use("/users", users);
 router.use("/objects", objects);
-router.use("/images", pics);
+router.use("/images", images);
+router.use("/data-export", dataExport);
+router.use("/data-share", dataShare);
 
 export default router;
