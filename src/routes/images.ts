43241eb73a004c6dfb0991b009b20816f48b40fb
Antti Jankeri <antti.jankeri@gmail.com>
2023-11-20 16:06:27 +0100

    feat: object api routes + search

--- src/routes/images.ts
+++ src/routes/images.ts
@@ -63,13 +63,12 @@ router.delete("/:id", async (req, res) => {
 
 router.patch("/:id", async (req, res) => {
   const body = req.body;
-  const updateObject = { $set: body };
-
   const validate = validateImagePartial(body);
   if (!validate.success) {
     return res.status(400).json(validate.error);
   }
 
+  const updateObject = { $set: body };
   const db = getImagesDb();
   const attempt = await db
     .collection("Test_images")
--- src/routes/objects.ts
+++ src/routes/objects.ts
@@ -1,29 +1,117 @@
 import express from "express";
+import { getObjectsDb } from "../db";
+import { ObjectId } from "mongodb";
+import { validateData, validateDataPartial } from "../data_types/validation";
+
+interface LooseObject {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [key: string]: any;
+}
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("COOL GET");
+router.get("/", async (req, res) => {
+  const db = getObjectsDb();
+  const cursor = db.collection("Test_objects").find();
+  const data = [];
+
+  for await (const item of cursor) {
+    data.push(item);
+  }
+
+  if (data) {
+    return res.json(data);
+  }
+
+  res.status(404).send("Not found");
 });
 
-router.get("/search", (req, res) => {
-  res.send("COOL SEARCH");
+router.get("/search", async (req, res) => {
+  const query = req.query;
+  const queryObject = {} as LooseObject;
+
+  Object.keys(req.query).forEach((key) => {
+    const value = query[key];
+    if (value) {
+      return (queryObject[key] = {
+        $regex: new RegExp(query[key] as string, "i"),
+      });
+    }
+  });
+
+  const db = getObjectsDb();
+  const cursor = db.collection("Test_objects").find(queryObject);
+  const data = [];
+
+  for await (const item of cursor) {
+    data.push(item);
+  }
+
+  if (data) {
+    return res.json(data);
+  }
+
+  res.status(404).send("Not found");
 });
 
-router.get("/:id", (req, res) => {
-  res.send("COOL ID");
+router.get("/:id", async (req, res) => {
+  const db = getObjectsDb();
+  const data = await db
+    .collection("Test_objects")
+    .findOne({ _id: new ObjectId(req.params.id) });
+
+  if (data) {
+    return res.json(data);
+  }
+
+  res.status(404).send("Not found");
 });
 
-router.post("/", (req, res) => {
-  res.send("COOL POST");
+router.post("/", async (req, res) => {
+  const body = req.body;
+  body.dateAdded = new Date();
+
+  const validate = validateData(body);
+  if (!validate.success) {
+    return res.status(400).json(validate.error);
+  }
+
+  const db = getObjectsDb();
+  await db.collection("Test_objects").insertOne(body);
+  res.status(201).json(body);
 });
 
-router.delete("/:id", (req, res) => {
-  res.send("COOL DELETE");
+router.delete("/:id", async (req, res) => {
+  const db = getObjectsDb();
+  const attempt = await db
+    .collection("Test_objects")
+    .deleteOne({ _id: new ObjectId(req.params.id) });
+
+  if (attempt.deletedCount === 1) {
+    return res.status(204).send("Deleted");
+  }
+
+  res.status(404).send("Not found");
 });
 
-router.patch("/:id", (req, res) => {
-  res.send("COOL UPDATE");
+router.patch("/:id", async (req, res) => {
+  const body = req.body;
+  const validate = validateDataPartial(body);
+  if (!validate.success) {
+    return res.status(400).json(validate.error);
+  }
+
+  const updateObject = { $set: body };
+  const db = getObjectsDb();
+  const attempt = await db
+    .collection("Test_objects")
+    .updateOne({ _id: new ObjectId(req.params.id) }, updateObject);
+
+  if (attempt.matchedCount === 1) {
+    return res.status(204).send("Updated");
+  }
+
+  res.status(404).send("Not found");
 });
 
 export default router;
