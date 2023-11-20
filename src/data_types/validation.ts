804c3609e590bb0dc8b545b3c6a688e686e3d0b5
Antti Jankeri <antti.jankeri@gmail.com>
2023-11-20 15:37:02 +0100

    feat: all image api paths w/o auth

--- src/data_types/validation.ts
+++ src/data_types/validation.ts
@@ -9,20 +9,32 @@ export const DataObjectSchema = ConfigObjectSchema.extend({
 
 export const DataObjectSchemaStrict = DataObjectSchema.strict();
 
+export const DataObjectSchemaPartial = DataObjectSchemaStrict.partial();
+
 export type DataObject = z.infer<typeof DataObjectSchemaStrict>;
 
 export const ImageDataSchema = dataTypes.ImageData;
 
+export const ImageDataSchemaPartial = ImageDataSchema.partial();
+
 export type ImageObject = z.infer<typeof ImageDataSchema>;
 
 export const validateData = (data: DataObject) => {
-  return DataObjectSchema.safeParse(data).success;
+  return DataObjectSchema.safeParse(data);
 };
 
 export const validateDataStrict = (data: DataObject) => {
-  return DataObjectSchemaStrict.safeParse(data).success;
+  return DataObjectSchemaStrict.safeParse(data);
+};
+
+export const validateDataPartial = (data: DataObject) => {
+  return DataObjectSchemaPartial.safeParse(data);
 };
 
 export const validateImage = (image: ImageObject) => {
-  return ImageDataSchema.safeParse(image).success;
+  return ImageDataSchema.safeParse(image);
+};
+
+export const validateImagePartial = (image: ImageObject) => {
+  return ImageDataSchemaPartial.safeParse(image);
 };
--- src/routes/images.ts
+++ src/routes/images.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { getImagesDb } from "../db";
 import { ObjectId } from "mongodb";
+import { validateImage, validateImagePartial } from "../data_types/validation";
 
 const router = express.Router();
 
@@ -8,10 +9,16 @@ router.get("/", async (req, res) => {
   const db = getImagesDb();
   const cursor = db.collection("Test_images").find();
   const data = [];
+
   for await (const item of cursor) {
     data.push(item);
   }
-  res.json(data);
+
+  if (data) {
+    return res.json(data);
+  }
+
+  res.status(404).send("Not found");
 });
 
 router.get("/:id", async (req, res) => {
@@ -19,19 +26,60 @@ router.get("/:id", async (req, res) => {
   const data = await db
     .collection("Test_images")
     .findOne({ _id: new ObjectId(req.params.id) });
-  res.json(data);
+
+  if (data) {
+    return res.json(data);
+  }
+
+  res.status(404).send("Not found");
 });
 
-router.post("/", (req, res) => {
-  res.send("COOL POST");
+router.post("/", async (req, res) => {
+  const body = req.body;
+  body.dateAdded = new Date();
+
+  const validate = validateImage(body);
+  if (!validate.success) {
+    return res.status(400).json(validate.error);
+  }
+
+  const db = getImagesDb();
+  await db.collection("Test_images").insertOne(body);
+  res.status(201).json(body);
 });
 
-router.delete("/:id", (req, res) => {
-  res.send("COOL DELETE");
+router.delete("/:id", async (req, res) => {
+  const db = getImagesDb();
+  const attempt = await db
+    .collection("Test_images")
+    .deleteOne({ _id: new ObjectId(req.params.id) });
+
+  if (attempt.deletedCount === 1) {
+    return res.status(204).send("Deleted");
+  }
+
+  res.status(404).send("Not found");
 });
 
-router.patch("/:id", (req, res) => {
-  res.send("COOL UPDATE");
+router.patch("/:id", async (req, res) => {
+  const body = req.body;
+  const updateObject = { $set: body };
+
+  const validate = validateImagePartial(body);
+  if (!validate.success) {
+    return res.status(400).json(validate.error);
+  }
+
+  const db = getImagesDb();
+  const attempt = await db
+    .collection("Test_images")
+    .updateOne({ _id: new ObjectId(req.params.id) }, updateObject);
+
+  if (attempt.matchedCount === 1) {
+    return res.status(204).send("Updated");
+  }
+
+  res.status(404).send("Not found");
 });
 
 export default router;
